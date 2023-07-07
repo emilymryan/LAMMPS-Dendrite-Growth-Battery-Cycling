4b0a7c21ecc59c8bcf72bfe57b788082c2bacfac
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2023-07-07 13:50:18 -0400

    Add files via upload
    Input files for constant charging under varying conditions

---
+++ ConstantCharging/submit-dend1.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -l
+#$ -l h_rt=120:00:00
+#$ -P ryanlab
+#$ -m bea
+#$ -j y
+#$ -N bvCC1
+#$ -o /projectnb/ryanlab/tmelsh/chargingProfiles/final/results/bvCC/$JOB_NAME.o$JOB_ID 
+#$ -e /projectnb/ryanlab/tmelsh/chargingProfiles/final/results/bvCC/$JOB_NAME.e$JOB_ID 
+#$ -pe mpi_16_tasks_per_node 64
+module load openmpi/3.1.1
+module load python3/3.6.5
+
+lmp=/projectnb/ryanlab/mmorey/mylammps/src/lmp_mpi
+
+output=/projectnb/ryanlab/tmelsh/chargingProfiles/final/results/bvCC/${JOB_NAME}_${JOB_ID}
+mkdir -p ${output}
+dname=${output}
+
+mpirun -np $NSLOTS ${lmp} -in ${JOB_NAME}_input.lmp -var dname ${dname}
+
+exit
+
